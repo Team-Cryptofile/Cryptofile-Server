94eccba3c03a2c7da5140de02d764c59bd74276a
Frode <frodeluren@gmail.com>
2019-11-15 01:00:34 +0100

    Successfully downloaded a file with get request
    And cleaned up some code

--- src/main/java/net/cryptofile/server/Repositories/MainRepository.java
+++ src/main/java/net/cryptofile/server/Repositories/MainRepository.java
@@ -2,15 +2,7 @@ package net.cryptofile.server.Repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import net.cryptofile.server.Objects.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.UUID;
 
 @Repository
 public class MainRepository {
@@ -24,19 +16,6 @@ public class MainRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public String addCryptofile(MultipartFile multipartFile, String title) throws IOException {
-        byte[] file = multipartFile.getBytes();
-        UUID uuid = UUID.randomUUID();
-        String uuidString = uuid.toString();
-        if(title.isEmpty()){
-            addFileToDb(uuidString, file);
-        }else{
-            addFileToDb(uuidString, file, title);
-        }
-
-        return uuidString;
-    }
-
     public byte[] getCryptofile(String fileId){
         String query = "SELECT cryptofile FROM cryptofiles WHERE file_info_idfile_info=?";
         byte[] file;
@@ -45,36 +24,7 @@ public class MainRepository {
         return file;
     }
 
-    public void addFileToDb(String uuid, byte[] file, String title){
-        String query = "SET @generatedID = UUID_TO_BIN(?);" +
-                "INSERT INTO cryptofiles (file_info_idfile_info, cryptofile) VALUES (@generatedID, ?);" +
-                "INSERT INTO file_info (idfile_info, file_name, time_added, time_deletes) VALUES (@generatedID, ?, NOW(), NOW() + INTERVAL 3 MONTH);" +
-                "INSERT INTO users_has_file_info (users_idusers, file_info_idfile_info) VALUES (?, @generatedID)";
-
-        try {
-            //long userId = getCurrentUser().getId();
-            long userId = 1;
-            jdbcTemplate.update(query, uuid, file, title, userId);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void addFileToDb(String uuid, byte[] file){
-        String query = "SET @generatedID = UUID_TO_BIN(?);" +
-                "INSERT INTO cryptofiles (file_info_idfile_info, cryptofile) VALUES (@generatedID, ?);" +
-                "INSERT INTO file_info (idfile_info, file_name, time_added, time_deletes) VALUES (@generatedID, @generatedID, NOW(), NOW() + INTERVAL 3 MONTH);" +
-                "INSERT INTO users_has_file_info (users_idusers, file_info_idfile_info) VALUES (?, @generatedID)";
-
-        try {
-            //long userId = getCurrentUser().getId();
-            long userId = 1;
-            jdbcTemplate.update(query, uuid, file, userId);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
+    /*
     private User getCurrentUser() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String loggedInUsername;
@@ -85,4 +35,6 @@ public class MainRepository {
         }
         return userRepository.findByUsername(loggedInUsername);
     }
+
+     */
 }
--- src/main/java/net/cryptofile/server/RestControllers/MainRestController.java
+++ src/main/java/net/cryptofile/server/RestControllers/MainRestController.java
@@ -8,18 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 @RestController
 public class MainRestController {
 
-    private final MainRepository mainRepository;
-    private FileService fileService;
-
     @Autowired
-    public MainRestController(MainRepository mainRepository){
-        this.mainRepository = mainRepository;
-    }
+    private FileService fileService;
 
     @PostMapping(value = "/add")
     public String addFile(@RequestParam("file") byte[] file,
@@ -31,7 +24,7 @@ public class MainRestController {
     @GetMapping(value = "/get/{fileId:[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}")
     @ResponseBody
     public ResponseEntity<ByteArrayResource> serveFile(@PathVariable String fileId) {
-        ByteArrayResource file = new ByteArrayResource(mainRepository.getCryptofile(fileId));
+        ByteArrayResource file = new ByteArrayResource(fileService.getCryptoFile(fileId));
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; fileId=\"" + fileId + "\"").body(file);
     }
